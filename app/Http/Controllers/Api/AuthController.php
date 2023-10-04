8ff6b979795f69f27a214c1b29be6ce73760feab
Arman Omerović <oarman93sk@gmail.com>
2023-10-05 01:48:54 +0200

    added api endpoints for login and register

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            "name" => ['required', 'max:255'],
+            "surname" => ['required', 'max:255'],
+            "email" => ['required', 'email', 'max:255', 'unique:users'],
+            "username" => ['required', 'max:255', 'unique:users', 'alpha_dash'],
+            "password" => ['required', 'confirmed', 'min:8', 'max:255']
+        ]);
+
+        if ($validator->fails()) {
+            return new JsonResponse($validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+
+        $user = new User();
+
+        $user->username = $request->username;
+        $user->first_name = $request->name;
+        $user->last_name = $request->surname;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+
+        $user->save();
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+
+            $token = $user->createToken(Str::random(40))->plainTextToken;
+
+            return new JsonResponse([
+                "success" => true,
+                "message" => "Register success!",
+                "data" => ["token" => $token]
+            ], 200);
+
+        } else {
+            return new JsonResponse('Invalid email or password', Response::HTTP_UNAUTHORIZED);
+        }
+    }
+
+    public function login(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            "email" => ['required', 'email', 'max:255'],
+            "password" => ['required', 'min:8', 'max:255']
+        ]);
+
+        if ($validator->fails()) {
+            return new JsonResponse($validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+
+            $user = Auth::user();
+            $token = $user->createToken(Str::random(40))->plainTextToken;
+
+            return new JsonResponse([
+                "success" => true,
+                "message" => "Login success!",
+                "data" => ["token" => $token]
+            ], 200);
+
+        } else {
+            return new JsonResponse([
+                "success" => false,
+                "message" => "Invalid email or password!",
+                "data" => []
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login')->name('api-login');
+    Route::post('register', 'register')->name('api-register');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
