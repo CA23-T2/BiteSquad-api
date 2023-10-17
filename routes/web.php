de2955bef7bdbc8f2ce4644d158bac2d67893db2
Arman Omerović <oarman93sk@gmail.com>
2023-10-17 21:57:32 +0200

    added AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,11 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
