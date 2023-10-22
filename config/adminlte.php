b92a64494a58886865316517c989f0fd7d081d18
Arman Omerović <oarman93sk@gmail.com>
2023-10-22 19:34:43 +0200

    password reset works

--- config/adminlte.php
+++ config/adminlte.php
@@ -257,7 +257,7 @@ return [
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => false,
-    'password_reset_url' => false,
+    'password_reset_url' => 'password/reset',
     'password_email_url' => 'password/email',
     'profile_url' => false,
 
